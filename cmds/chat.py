e6788c2910602cc5495211866486dc741b12cd02
xiaojie4082 <xiaojie4082@gmail.com>
2024-09-26 18:15:29 +0800

    feat: 新增錯誤回報功能並更新連結
    - 修改 chat.py，新增錯誤回報功能並更新連結
    - 將回應訊息寫入 "sqlite" 資料庫
    - 建立三個狀態按鈕：resolved、unresolved、report
    - 更新 README.md

--- cmds/chat.py
+++ cmds/chat.py
@@ -7,6 +7,7 @@ from core.classes import Cog_Extension
 import requests
 import csv
 import os
+import sqlite3
 
 class Chat(Cog_Extension):
     def __init__(self, bot):
@@ -26,19 +27,34 @@ class Chat(Cog_Extension):
     ):
         message = await ctx.respond('正在處理中...')
         response = requests.post('http://localhost:5000/puchat', json={'message':訊息})
+        if response.status_code != 200:
+            await message.edit_original_response(content="發生錯誤，請稍後再試")
+        # 取得 user_id
+        user_id = ctx.author.id
+
+        # 將回應訊息寫入"sqlite"資料庫，[id, 訊息, 回應, user_id, 時間, 狀態]
+        conn = sqlite3.connect("data/chat.db")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS chat (id INTEGER PRIMARY KEY AUTOINCREMENT, 訊息 TEXT, 回應 TEXT, user_id INTEGER, 時間 TEXT, 狀態 TEXT)")
+        cur.execute("INSERT INTO chat (訊息, 回應, user_id, 時間, 狀態) VALUES (?, ?, ?, datetime('now', 'localtime'), 'no_reply_yet')", (訊息, response.text, user_id))
+        conn.commit()
+        # 紀錄此次對話的id
+        chat_id = cur.lastrowid
+        conn.close()
+
         view = discord.ui.View()
-        button = discord.ui.Button(label="回報錯誤", style=discord.ButtonStyle.danger)
-        async def button_callback(interaction):
-            if not os.path.exists("error_reports.csv"):
-                with open("data/chat_error_reports.csv", "w", newline='', encoding="utf-8") as f:
-                    writer = csv.writer(f)
-                    writer.writerow(["訊息", "回應"])
-            with open("data/chat_error_reports.csv", "a", newline='', encoding="utf-8") as f:
-                writer = csv.writer(f)
-                writer.writerow([訊息, response.text])
-            await interaction.response.send_message("已回報錯誤，謝謝！", ephemeral=True)
-        button.callback = button_callback
-        view.add_item(button)
+        # 建立三個狀態按鈕(resolved, unresolved, report)
+        button_reprot = discord.ui.Button(label="回報錯誤", style=discord.ButtonStyle.danger)
+        async def button_reprot_callback(interaction, chat_id=chat_id):
+            conn = sqlite3.connect("data/chat.db")
+            cur = conn.cursor()
+            cur.execute("UPDATE chat SET 狀態 = 'reported' WHERE id = ?", (chat_id,))
+            conn.commit()
+            conn.close()
+            message_text = "我們已收到您的錯誤已回報，回報編號為 " + chat_id + "。如有任何疑問，請提供此編號與管理員聯繫，謝謝您！"
+            await interaction.response.send_message(message_text, ephemeral=True)
+        button_reprot.callback = button_reprot_callback
+        view.add_item(button_reprot)
         await message.edit_original_response(content=response.text, view=view)
 
 def setup(bot):
